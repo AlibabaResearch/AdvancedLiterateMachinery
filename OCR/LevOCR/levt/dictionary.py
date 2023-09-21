d6b18bec0e50c9bac39bb5a5e6df513498da7d23
yue kun <yuekun.wp@alibaba-inc.com>
2023-09-21 20:28:37 +0800

    fix copyright conflicts

--- OCR/LevOCR/levt/dictionary.py
+++ OCR/LevOCR/levt/dictionary.py
@@ -1,3 +1,8 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 import os
 from collections import Counter
 from multiprocessing import Pool
